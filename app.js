ebd1c792e80727089996a2fc91f10ab11343d7ca
Marce Serrano <smarcelo75@hotmail.com>
2022-01-25 18:12:58 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require('colors');
+const { argv } = require('./config/yargs');
+const { grabarArchivo, eliminarRegistro } = require('./archivos/archivos');
+
+const persona = {
+    nombre: argv.nombre,
+    apellido: argv.apellido,
+    edad: argv.edad
+};
+
+let nombreArchivo = '';
+
+if (persona.edad >= 45)
+    nombreArchivo = 'mayores45.txt';
+else
+    nombreArchivo = 'menores45.txt';
+
+if (!argv.delete)
+    grabarArchivo(nombreArchivo, persona);
+else
+    eliminarRegistro(nombreArchivo, persona);

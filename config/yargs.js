2d4454ee1bbc698eb9a32bb30c0aa931bce43826
Marce Serrano <smarcelo75@hotmail.com>
2022-01-26 21:08:13 -0300

    dev: correcciones de eslint

--- config/yargs.js
+++ config/yargs.js
@@ -1,36 +1,35 @@
-const argv = require('yargs')
+const { argv } = require('yargs')
     .option('nombre', {
         alias: 'n',
         description: 'Nombre',
-        demandOption: true
+        demandOption: true,
     })
     .option('apellido', {
         alias: 'a',
         description: 'Apellido',
-        demandOption: true
+        demandOption: true,
     })
     .option('edad', {
         alias: 'e',
         description: 'Edad',
         demandOption: true,
-        type: 'number'
+        type: 'number',
     })
     .option('delete', {
         alias: 'd',
         description: 'Elimina el registro del archivo correspondiente de acuerdo a la edad',
         demandOption: true,
         type: 'boolean',
-        default: false
+        default: false,
     })
-    .check((argv) => {
-        if (isNaN(argv.edad)) {
+    .check((parametros) => {
+        if (Number.isNaN(parametros.edad)) {
             throw Error('La edad debe ser un valor numérico');
         }
-        if (argv.edad < 18) {
+        if (parametros.edad < 18) {
             throw Error('La edad debe ser mayor a 18 años');
         }
         return true;
-    })
-    .argv
+    });
 
-module.exports = { argv }
+module.exports = { argv };
